55ab94023cd61f9694f6edae32eccbf743534733
Julie Chen <julie.chen@mail.mcgill.ca>
2023-05-17 15:58:17 -0400

    Add link component navigation for blog.

--- hello-julie/pages/blog/index.js
+++ hello-julie/pages/blog/index.js
@@ -1,11 +1,17 @@
+import Link from 'next/link';
+
 function BlogPostList() {
-    return ( 
-        <>
-            <h2>Blog Post 1</h2>
-            <h2>Blog Post 2</h2>
-            <h2>Blog Post 3</h2>
-        </>
-    )
+  return (
+    <>
+      <h2>Blog Post 1</h2>
+      <h2>Blog Post 2</h2>
+      <h2>Blog Post 3</h2>
+
+      <Link href='/' legacyBehavior>
+        <a>Home</a>
+      </Link>
+    </>
+  );
 }
 
-export default BlogPostList
+export default BlogPostList;
--- hello-julie/pages/index.js
+++ hello-julie/pages/index.js
@@ -1,5 +1,22 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 function Home() {
-  return <h1>Home Page</h1>
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log('Creating your post...');
+    router.push('/blog');
+  };
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href='/blog' legacyBehavior>
+        <a>Blog</a>
+      </Link>
+      <button onClick={handleClick}>Create Blog Post</button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
